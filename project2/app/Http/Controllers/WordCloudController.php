b94fee1efcbc05d5d16886f8c51a6bee6f3e4b68
student <mhamasak@usc.edu>
2017-04-03 02:01:49 -0700

    html tags for words

--- project2/app/Http/Controllers/WordCloudController.php
+++ project2/app/Http/Controllers/WordCloudController.php
@@ -13,6 +13,7 @@ class WordCloudController extends Controller
     $fontSizeString = "; font-size:";
     $linkString = "px;' href='http://localhost:8000/list/" . $scholarOrKeyword . "/";
     $colors = array("red", "blue", "green", "purple", "yellow", "black", "orange", "gray", "brown", "magenta", "skyblue", "forestgreen", "aliceblue", "salmon");
+    $idTagString = "' id='word";
 
     $shuffled_array = array();
     $shuffled_keys = array_keys($wordList);
@@ -21,13 +22,15 @@ class WordCloudController extends Controller
       $shuffled_array[$shuffled_key] = $wordList[$shuffled_key];
     }
 
+    $i=0;
     foreach ($shuffled_array as $key => $value){
+      $i++;
       $color = $colors[array_rand($colors, 1)];
       //echo $color;
       $fontSize = $value * 6;
       if ($fontSize > 40) $fontSize = 40;
       if ($fontSize < 12) $fontSize = 10;
-      $toAdd = $startingATag . $color . $fontSizeString . $fontSize . $linkString . $searchTerm. "/" . $key . "'> " . $key . " </a>";
+      $toAdd = $startingATag . $color . $fontSizeString . $fontSize . $linkString . $searchTerm. "/" . $key . $idTagString . $i . "'> " . $key . " </a>";
       $wordCloudString = $wordCloudString . $toAdd;
     }
 
