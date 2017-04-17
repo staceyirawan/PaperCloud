a166ba732224ebd19517c58a41c1efea027c0d41
student <mhamasak@usc.edu>
2017-04-17 01:52:58 -0700

    comments

---
+++ project2/tests/Unit/GetACMPapersTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use App\Http\Controllers\PaperController;
+
+
+class GetACMPapersTest extends TestCase
+{
+    /**
+     * 
+     * @covers App\Http\Controllers\PaperController::getAllInfoFromHTML
+     */
+    public function testGetACMPaperInfoFromtURLS() {
+		$this->assertEquals(1, 1);
+    }
+
+	
+}
