9a127faeb108daf92346b3731f10b5ed47aa4566
enozservant <enochhan@usc.edu>
2017-04-17 08:50:42 -0700

    Begin pair programming with Lucy in HTML/CSS bibtexs

---
+++ project2/resources/views/errorhandle.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        
+</head>
+
+
+
+ <body>
+    
+    
+    <div id = "searchCloud">
+<?php
+    echo $wordCloudString;
+    ?>=
+
+</div>
+</body>
